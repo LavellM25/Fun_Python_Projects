4fd7854572af39791db8a8e8cc5dd18ec0c3529d
LavellM25 <Lavellmcgrone25@gmail.com>
2025-01-18 17:59:00 -0600

    EP2.py

---
+++ Python_Content/EP2.py
@@ -0,0 +1,26 @@
+# Simple math operators (+, -, *, /) Addition, Subtraction, Multiplication, Division
+# Assigning variables
+
+
+
+# Assigning variables
+number1 = 20
+number2 = 6
+
+
+# Performing math operations
+sum_result = number1 + number2       # Addition
+difference = number1 - number2       # Subtraction
+product = number1 * number2          # Multiplication
+quotient = round(float(number1 / number2),3)        # Division
+
+# float = 4.5
+# Integers = whole # ex: 5
+
+# Displaying the results
+print(sum_result)       # Outputs 20
+print("Subtraction:", difference)    # Outputs 10
+print("Multiplication:", product)    # Outputs 75
+print("Division:", quotient)         # Outputs 3.0
+
+# print((quotient))
